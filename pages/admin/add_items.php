dcba93c5ef0510a32200d3a93d5bec304980d74d
Ionuț Popescu <Ionu Popescu>
2017-01-23 01:03:47 +0200

    Added sockets at item-shop

--- pages/admin/add_items.php
+++ pages/admin/add_items.php
@@ -12,6 +12,20 @@
 			for($i=0;$i<=7;$i++) 
 				if($_POST['applytype'.$i]==0)
 					$_POST['applyvalue'.$i]=0;
+				
+		if($_POST['socket0']!="")
+			$socket0 = $_POST['socket0'];
+		else
+			$socket0 = 0;
+		if($_POST['socket1']!="")
+			$socket1 = $_POST['socket1'];
+		else
+			$socket1 = 0;
+		if($_POST['socket2']!="")
+			$socket2 = $_POST['socket2'];
+		else
+			$socket2 = 0;
+
 		if(check_item_column("applytype0") && check_item_sash($_POST['vnum']) && $_POST['time2']==0)
 		{
 			$stmt = $sqlite->prepare('INSERT INTO item_shop_items (category, description, pay_type, coins, count, vnum, socket1, socket2, attrtype0, attrvalue0, attrtype1 , attrvalue1, attrtype2, attrvalue2, attrtype3, attrvalue3, attrtype4, attrvalue4, attrtype5, attrvalue5, attrtype6, attrvalue6, applytype0, applyvalue0, applytype1, applyvalue1, applytype2, applyvalue2, applytype3, applyvalue3, applytype4, applyvalue4, applytype5, applyvalue5, applytype6, applyvalue6, applytype7, applyvalue7) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
@@ -45,6 +59,25 @@
 								$_POST['applytype3'], $_POST['applyvalue3'], $_POST['applytype4'], $_POST['applyvalue4'], $_POST['applytype5'], $_POST['applyvalue5'], 
 								$_POST['applytype6'], $_POST['applyvalue6'], $_POST['applytype7'], $_POST['applyvalue7']));
 		}
+		else if(check_item_column("applytype0") && ($socket0 || $socket1 || $socket2))//pietre
+		{
+			$stmt = $sqlite->prepare('INSERT INTO item_shop_items (category, description, pay_type, coins, count, vnum, socket0, socket1, socket2, attrtype0, attrvalue0, attrtype1 , attrvalue1, attrtype2, attrvalue2, attrtype3, attrvalue3, attrtype4, attrvalue4, attrtype5, attrvalue5, attrtype6, attrvalue6, applytype0, applyvalue0, applytype1, applyvalue1, applytype2, applyvalue2, applytype3, applyvalue3, applytype4, applyvalue4, applytype5, applyvalue5, applytype6, applyvalue6, applytype7, applyvalue7) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+			$stmt->execute(array($get_category, $_POST['description'], $_POST['method_pay'], $_POST['coins'], $_POST['count'], $_POST['vnum'], $socket0, $socket1, $socket2,
+								$_POST['attrtype0'], $_POST['attrvalue0'], $_POST['attrtype1'], $_POST['attrvalue1'], $_POST['attrtype2'], $_POST['attrvalue2'], 
+								$_POST['attrtype3'], $_POST['attrvalue3'], $_POST['attrtype4'], $_POST['attrvalue4'], $_POST['attrtype5'], $_POST['attrvalue5'], 
+								$_POST['attrtype6'], $_POST['attrvalue6'], 
+								$_POST['applytype0'], $_POST['applyvalue0'], $_POST['applytype1'], $_POST['applyvalue1'], $_POST['applytype2'], $_POST['applyvalue2'], 
+								$_POST['applytype3'], $_POST['applyvalue3'], $_POST['applytype4'], $_POST['applyvalue4'], $_POST['applytype5'], $_POST['applyvalue5'], 
+								$_POST['applytype6'], $_POST['applyvalue6'], $_POST['applytype7'], $_POST['applyvalue7']));
+		}
+		else if($socket0 || $socket1 || $socket2)//pietre
+		{
+			$stmt = $sqlite->prepare('INSERT INTO item_shop_items (category, description, pay_type, coins, count, vnum, socket0, socket1, socket2, attrtype0, attrvalue0, attrtype1 , attrvalue1, attrtype2, attrvalue2, attrtype3, attrvalue3, attrtype4, attrvalue4, attrtype5, attrvalue5, attrtype6, attrvalue6) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+			$stmt->execute(array($get_category, $_POST['description'], $_POST['method_pay'], $_POST['coins'], $_POST['count'], $_POST['vnum'], $socket0, $socket1, $socket2,
+								$_POST['attrtype0'], $_POST['attrvalue0'], $_POST['attrtype1'], $_POST['attrvalue1'], $_POST['attrtype2'], $_POST['attrvalue2'], 
+								$_POST['attrtype3'], $_POST['attrvalue3'], $_POST['attrtype4'], $_POST['attrvalue4'], $_POST['attrtype5'], $_POST['attrvalue5'], 
+								$_POST['attrtype6'], $_POST['attrvalue6']));
+		}
 		else if($_POST['time2']==0)
 		{
 			$stmt = $sqlite->prepare('INSERT INTO item_shop_items (category, description, pay_type, coins, count, vnum, socket2, attrtype0, attrvalue0, attrtype1 , attrvalue1, attrtype2, attrvalue2, attrtype3, attrvalue3, attrtype4, attrvalue4, attrtype5, attrvalue5, attrtype6, attrvalue6) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
@@ -63,7 +96,7 @@
 ?>
 
 			<ul class="breadcrumb">
-				<li><a href="index.php">Item-Shop</a></li>
+				<li><a href="shop.php">Item-Shop</a></li>
 				<li class="active"><?php print ucfirst($current_page); ?></li>
 			</ul>
 			<div class="well">
@@ -164,6 +197,25 @@
 							</div>
 							
 							<?php } ?>
+							<div class="form-group">					
+								<a class="btn btn-primary" role="button" data-toggle="collapse" href="#sockets" aria-expanded="false" aria-controls="sockets">
+									Sockets
+								</a>
+								<div class="collapse" id="sockets">
+									<div class="form-group">
+										<label class="control-label" for="socket0">Socket (1)</label>
+										<input class="form-control" name="socket0" id="socket0" type="number" value="">
+									</div>
+									<div class="form-group">
+										<label class="control-label" for="socket1">Socket (2)</label>
+										<input class="form-control" name="socket1" id="socket1" type="number" value="">
+									</div>
+									<div class="form-group">
+										<label class="control-label" for="socket2">Socket (3)</label>
+										<input class="form-control" name="socket2" id="socket2" type="number" value="">
+									</div>
+								</div>
+							</div>
 							<div class="form-group">					
 								<a class="btn btn-primary" role="button" data-toggle="collapse" href="#time" aria-expanded="false" aria-controls="time">
 									<?php print $lang['item_time']; ?> (Min.)
